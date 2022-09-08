c2f2ca49c6473ecb76a80b11be56bf5d788da77f
berhanez <birhanez@gmail.com>
2022-09-08 04:45:48 -0700

    5-print.c prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - outputs with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	getchar();
+	return (0);
+}
+
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -7,3 +7,4 @@ created separate 1-main.c works
 2-asseembler generates assembly code
 exe cisfun is born
 wrote a C program that pringts a text in 4-puts.c
+C program with printf
