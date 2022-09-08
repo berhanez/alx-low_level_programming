cf99068dffe9f27b4aff13a6dc31f19a08c60b64
berhanez <birhanez@gmail.com>
2022-09-08 06:16:45 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-/**                                                                           
- * main - outputs size of different types
+/**                                                                                                                         
  *
- * Return: Always 0
- */
-int main()
-{ 
+ *  * main - outputs size of different types                                                                                   
+ *
+ *   *                                                                                                                          
+ *
+ *    * Return: Always 0                                                                                                         
+ *
+ *     */
+int main(void)
+{
 	char CharType;
 	int IntegerType;
-	long int LongIntType;
-	long long int LongLongIntType;
+	long LongType;
+	long long LongLongType;
 	float FloatType;
 
-	printf("Size of a char: %lu byte(s)\n",sizeof(CharType));
-	printf("Size of an int: %lu byte(s)\n",sizeof(IntegerType));
-	printf("Size of a long int: %lu byte(s)\n",sizeof(LongIntType));
-	printf("Size of a long long int: %lu byte(s)\n",sizeof(LongLongIntType);
-	printf("Size of a float: %lu byte(s)\n",sizeof(FloatType));
-        getchar();
-       	return (0);
-}	
+	printf("Size of a char: %ld byte(s)\n",sizeof(CharType));
+	printf("Size of an int: %ld byte(s)\n",sizeof(IntegerType));
+	printf("Size of a long int: %ld byte(s)\n",sizeof(LongIntType));
+	printf("Size of a long long int: %ld byte(s)\n",sizeof(LongLongIntType);
+	printf("Size of a float: %ld byte(s)\n",sizeof(FloatType));
+	return (0);
+	}
