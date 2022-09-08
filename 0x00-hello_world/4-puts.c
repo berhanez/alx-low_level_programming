a53112a4897d3db03dae727fe705649bf2b921b2
berhanez <birhanez@gmail.com>
2022-09-08 04:16:44 -0700

    second fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ *main - Outputs text
+ *
+ *Return- Always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 getchar();
-return(0);
+return (0);
 }
 
