58aa7c6cebbcc2ca8a364faee8f3678eaa5262aa
berhanez <birhanez@gmail.com>
2022-09-09 05:27:17 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - alphabet in lower case
+ * Description: print alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+return (0);
+}
+
