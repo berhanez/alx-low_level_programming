b998e15a8aa504267e6a6b3041bc053d89862ce0
berhanez <birhanez@gmail.com>
2022-09-13 05:19:17 -0700

    Absolute value task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description: absolute value
+ * Return: n
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
