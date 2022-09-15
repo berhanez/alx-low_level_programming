73a5b61ca36f94c23bcffba95e40b46bd2edeb0e
berhanez <birhanez@gmail.com>
2022-09-15 09:30:39 -0700

    changed syntax in file 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,20 +1,13 @@
 #include "main.h"
 
 /**
+ * print_remaining_days - takes a date and prints how many days
+ * are left in the year, taking leap years into account
  *
- * * print_remaining_days - takes a date and prints how many days are
- *
- * *		left in the year, taking leap years into account
- *
- * *
- *
- * * @month: month in number format
- *
- * * @day: day of month
- *
- * * @year: year
- *
- * */
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -30,7 +23,7 @@ void print_remaining_days(int month, int day, int year)
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
-	
+
 	}
 	else
 	{
