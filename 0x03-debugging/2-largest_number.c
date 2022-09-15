42a708b6e75317f2ae350e150eabe86cade789a1
berhanez <birhanez@gmail.com>
2022-09-15 09:38:38 -0700

    completely restructuresd the formula to find largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
  * largest_number - returns the largest of 3 numbers
- *
  * @a: first integer
- *
  * @b: second integer
- *
  * @c: third integer
- *
  * Return: largest number
- *
  */
 
 int largest_number(int a, int b, int c)
@@ -18,15 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest = b;
 
-	if (a > b && b > c)
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
+	else if (c > b)
 	{
 		largest = c;
 	}
