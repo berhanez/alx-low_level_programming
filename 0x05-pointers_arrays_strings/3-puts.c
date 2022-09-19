b4ef6010c6c9a62a67f9ec52a3c6fe89dc287d23
berhanez <birhanez@gmail.com>
2022-09-19 09:29:52 -0700

    Task 3- puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by new line
+ * @str: string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
