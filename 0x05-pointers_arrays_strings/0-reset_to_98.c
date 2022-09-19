596be7c0db971d556aefccd9e787fc481fe8fccf
berhanez <birhanez@gmail.com>
2022-09-19 01:59:46 -0700

    Attempt 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates int value to 98
+ * Description: value update
+ * @n: carrier integer variable
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
