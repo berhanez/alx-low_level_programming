21ad5941e49d8dc3c029bdb614552e2dea2a85fc
berhanez <birhanez@gmail.com>
2022-09-22 10:21:48 -0700

    5-string_toupper converts small case to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - convert from lower to uppercase
+ * @str: string
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+			i++;
+		}
+	}
+	return (str);
+}
