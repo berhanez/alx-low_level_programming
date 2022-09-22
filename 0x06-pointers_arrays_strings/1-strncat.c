78f4b7734f3db52c17dc87d2a82981fdf6fefe28
berhanez <birhanez@gmail.com>
2022-09-22 08:51:40 -0700

    1-strncat.c concatenates string up to n from src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates strin
+ * @dest: string to be appended
+ * @src: string to be added to dest
+ * @n: n bytes at most used from src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, d_len = 0;
+
+	while (dest[i++])
+	{
+		d_len++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[d_len++] = src[i++];
+	}
+return (dest);
+}
