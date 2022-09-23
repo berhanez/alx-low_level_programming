7c67927cf4936052ba1d289510ec42fbbceee28f
berhanez <birhanez@gmail.com>
2022-09-23 09:03:22 -0700

    fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *s);
 void print_number(int n);
+int main(void);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /*MAIN_H*/
