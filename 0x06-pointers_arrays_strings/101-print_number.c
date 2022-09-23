c729d770d8bef50c0f96183988f50af855a8166c
berhanez <birhanez@gmail.com>
2022-09-23 08:07:25 -0700

    ERRORS fixed

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-##include "main.h"
+#include "main.h"
 /**
  * print_number - prints an integer
  * @n: integer
@@ -15,7 +15,7 @@ void print_number(int n)
 	}
 	if ((x / 10) > 0)
 	{
-		print_number(num / 10);
+		print_number(x / 10);
 	}
-	_putchar((num % 10) + '0');
+	_putchar((x % 10) + '0');
 }
