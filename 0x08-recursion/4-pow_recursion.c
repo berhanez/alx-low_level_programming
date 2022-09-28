42e8a0bd69f1d11360692da36463054900aef9fe
berhanez <birhanez@gmail.com>
2022-09-28 08:25:37 -0700

    Task 4 calculates power with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - return x**y
+ * @x: base number
+ * @y: exponent
+ * Return: -1 for y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+
