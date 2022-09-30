e400e8a86450ff436e1bb01ea2592cccf1fd7db0
berhanez <birhanez@gmail.com>
2022-09-30 05:29:57 -0700

    Betty errors fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - returns name of program
+ * Description: cmd argument to main
+ * @argc: counts cmd arguments
+ * @argv: holds arguments as string
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i = 0;
@@ -8,4 +14,4 @@ int main(int argc, char *argv[])
 	argc = i;
 	printf("%s\n", argv[argc]);
 	return (0);
-}	
+}
