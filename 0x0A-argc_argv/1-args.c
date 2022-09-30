1947d9716317fba337b97848913664adef294f05
berhanez <birhanez@gmail.com>
2022-09-30 05:49:39 -0700

    Task 1 : counts arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the number of args
+ * @argc: counts arguments
+ * @argv: holds cmd arguments as string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
