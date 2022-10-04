51322ca75ca4fc8649a0c314693b1824eb7589f3
berhanez <birhanez@gmail.com>
2022-10-04 02:08:15 -0700

    :Task 00 - create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
-	
+	unsigned int i = 0;
+
 	if (size == 0)
 		return (NULL);
-	else
+
 	str = malloc(sizeof(char) * size);
-	str[0] = c;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		str[i] = c;
+	}
 	return (str);
 }
