e0dc036f6a4a1afe22b67b35ee2303e495303b13
berhanez <birhanez@gmail.com>
2022-10-06 05:19:34 -0700

    betty fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n++;
 	s1s2 = malloc(sizeof(char) * (n + 1));
 
-	if(s1s2 == NULL)
+	if (s1s2 == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
