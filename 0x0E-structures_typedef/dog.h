8e86d9aabb638d876a176d88477c4028d7464ddb
berhanez <birhanez@gmail.com>
2022-10-10 10:36:58 -0700

    struct type error fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ struct dog
 };
 
 /* Lets define the typedef */
-typedef struct dog dog_struct;
+typedef struct dog dog_t;
 
 /* Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
