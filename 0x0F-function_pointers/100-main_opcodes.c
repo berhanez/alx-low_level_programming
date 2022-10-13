83ff5091b83d70c40d0bd6c7425bc29cdb47aec9
berhanez <birhanez@gmail.com>
2022-10-13 16:38:46 +0300

    Error fixed

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,31 +8,35 @@
  */
 int main(int argc, char *argv[])
 {
-	int nbytes, i;
-	char *ar;
+	int bytes, i;
+	int (*address)(int, char **) = main;
+	unsigned char op_code;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	nbytes = atoi(argv[1]);
+
+	bytes = atoi(argv[1]);
 	
-	if (nbytes < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	ar = (char *)main;
-
-	for (i = 0; i < nbytes; i++)
+	
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == nbytes - 1)
-		{
-			printf("%02hhx\n", ar[i]);
-			break;
-		}
-		printf("%02hhx ", ar[i]);
+		op_code = *(unsigned char *)address;
+		printf("%.2x", op_code);
+
+		if (i == bytes - 1)
+			continue;
+		printf(" ");
+		address++;
 	}
+	printf("\n");
+
 	return (0);
 }
