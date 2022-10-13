b0b90db11c66fe4361c8dbff468a13fa42889ab8
root <birhanez@gmail.com>
2022-10-13 06:18:44 -0700

    Betty errors fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
+/**
+ * sum_them_all - sums all arguments passed
+ * @n: number of elements to sum
+ * Return: 0 if n == NULL, else return sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list a;
 	unsigned int i;
 	int sum;
 
-	va_start (a, n);
+	va_start(a, n);
 
 	if (n == 0)
 		return (0);
