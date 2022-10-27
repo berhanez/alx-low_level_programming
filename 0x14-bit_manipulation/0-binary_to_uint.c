9e5d92336f92d5ee6519961b34a754cfd7f4dcf9
berhanez <birhanez@gmail.com>
2022-10-27 13:13:45 +0300

    Task 1 - print binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
- * Return: the converted number
+ * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
