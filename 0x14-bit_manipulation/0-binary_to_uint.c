ed75c6a8824b4afc65c8e7ede8d6028a3c383a1d
berhanez <birhanez@gmail.com>
2022-10-27 03:27:10 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,13 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-	
+
 	while (b[i])
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-	
-		uint = (2 * uint) + (b[i] - '0');
+
+		uint = (2 * uint)+(b[i] - '0');
 
 		i++;
 	}
