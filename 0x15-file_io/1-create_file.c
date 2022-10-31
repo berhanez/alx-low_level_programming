93857d0b4489b319fbb205d542eb2ad3837c770c
berhanez <birhanez@gmail.com>
2022-10-31 16:52:01 +0300

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rw, i = 0;
+	int fd, w, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (fd < 0)
-		exit(1);
-
-	if (text_content == NULL)
-		text_content = "";
-	while (text_content[i])
+	if (text_content != NULL)
+	{
+		while (text_content[i])
 		i++;
+	}
 
-	rw = write(fd, text_content, i);
-	if (rw == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, i);
+
+	if (fd == -1 || w == -1)
 		return (-1);
+
 	close(fd);
 
 	return (1);
