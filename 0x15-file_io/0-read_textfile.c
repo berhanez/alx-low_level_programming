2f75997a44e4693a3e56d6121a8328b0cec15105
berhanez <birhanez@gmail.com>
2022-10-31 16:22:05 +0300

    error fix again!

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buff;
-	ssize_t open, readd, writer;
+	ssize_t readd, writer;
 
 	if (filename == NULL)
 		return (0);
