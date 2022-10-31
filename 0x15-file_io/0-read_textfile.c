93af0971af610c2328e1a0bcf79e4fec6ed7e080
berhanez <birhanez@gmail.com>
2022-10-31 16:17:03 +0300

    Error fixed - used return instead of exit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,11 +10,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buff;
-	ssize_t readd, writer;
+	ssize_t open, readd, writer;
 
+	if (filename == NULL)
+		return (0);
+		
 	fd = open(filename, O_RDONLY);
 
-	if (fd == -1 || filename == NULL)
+	if (fd == -1)
 		return (0);
 
 	buff = malloc(sizeof(char) * letters);
@@ -36,6 +39,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buff);
 
 	if (readd != writer)
-		exit(1);
+		return (0);
 	return (writer);
 }
